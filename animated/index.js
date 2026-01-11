c1ccffcd70ef0218cb5b39581d004c8d4d598354
john9francis <john9francis@gmail.com>
2026-01-11 16:59:46 -0500

    Added a cute animated linkedin popup

--- animated/index.js
+++ animated/index.js
@@ -1,1 +1,37 @@
 const { animate } = anime;
+const utils = anime.utils
+const spring = anime.spring
+const createDraggable = anime.createDraggable
+
+const [ $logo ] = utils.$('.logo.js');
+const [ $button ] = utils.$('button');
+let rotations = 0;
+
+// Created a bounce animation loop
+animate('.logo.js', {
+  scale: [
+    { to: 1.25, ease: 'inOut(3)', duration: 200 },
+    { to: 1, ease: spring({ bounce: .7 }) }
+  ],
+  loop: true,
+  loopDelay: 250,
+});
+
+// Make the logo draggable around its center
+createDraggable('.logo.js', {
+  container: [10, 0, 0, 0],
+  releaseEase: spring({ bounce: .7 })
+});
+
+// Animate logo rotation on click
+const rotateLogo = () => {
+  rotations++;
+  $button.innerText = `rotations: ${rotations}`;
+  animate($logo, {
+    rotate: rotations * 360,
+    ease: 'out(4)',
+    duration: 1500,
+  });
+}
+
+$button.addEventListener('click', rotateLogo);
